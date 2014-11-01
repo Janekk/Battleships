826f5eb05180e2cf0fcbb2f319c351bed8a5312e
Janekk <jkacalak@gmail.com>
2014-11-01 21:05:53 +0100

    added new component for ships selection

---
+++ client/ShipsPanel.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var _ = require('lodash');
+
+var ShipsPanel = React.createClass({
+  render: function () {
+    return (
+      <div className="ships-panel">
+
+      </div>
+    );
+  }
+});
+
+module.exports = ShipsPanel;
