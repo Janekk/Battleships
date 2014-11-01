6dd557e150ddc39d5723e44b4e7adf3aca50e235
Rene <mail@reneschimmelpfennig.de>
2014-11-01 22:30:43 +0100

    implemented socket communication

--- client/socket-client.js
+++ client/socket-client.js
@@ -2,52 +2,30 @@ module.exports = function() {
     var socket = io();
 
     $(document).ready(function() {
-        var $messages = $('#messages');
         var $roomID = $('#room-id');
-        var $registerRoom = $('#register-room');
-        var $sendMessage = $('#send-message');
-        var $message = $('#message');
+        var $joinButton = $('#join-button');
 
-        $registerRoom.on('click', function(e) {
+        $joinButton.on('click', function(e) {
             // disable buttons
             $roomID.prop('disabled', true);
-            $registerRoom.prop('disabled', true);
+            $joinButton.prop('disabled', true);
 
-            socket.emit('register room', $roomID.val());
-        });
-
-        $sendMessage.on('click', function(e) {
-            socket.emit('chat message', $message.val());
-            $message.val('');
+            socket.emit('join room', $roomID.val());
         });
     });
 
-    socket.on('room registered', function(result) {
-        var $messages = $('#messages');
-
+    socket.on('room joined', function(result) {
         if (result.isSuccessful) {
-            $messages.append($('<li>')
-                .addClass('info')
-                .text('room registration: successful'));
-
-            // activate message field
-            $('#message').prop('disabled', false);
-            $('#send-message').prop('disabled', false);
+            if (result.message) {
+                toastr.info(result.message);
+            }
         }
         else { // error
-            $messages.append($('<li>')
-                .addClass('error')
-                .text(result.error));
+            toastr.error(result.error);
 
             // activate buttons
             $('#room-id').prop('disabled', false);
-            $('#register-room').prop('disabled', false);
+            $('#join-button').prop('disabled', false);
         }
-    });
-
-    socket.on('chat message', function(msg){
-        $('#messages').append($('<li>')
-            .addClass('chat')
-            .text(msg));
     });
 };
--- socket-server.js
+++ socket-server.js
@@ -2,26 +2,33 @@ module.exports = function(http) {
     var io = require('socket.io')(http);
     var _ = require('lodash');
 
-    io.on('connection', function(socket){
+    io.on('connection', function(socket) {
         var roomJoined = false;
 
         console.log('a user connected');
 
-        socket.on('register room', function(roomID) {
+        socket.on('join room', function(roomID) {
             console.log('join room', roomID);
 
             var sockets = getSocketsOfRoom(roomID);
-            if (sockets.length > 1) {
-                return socket.emit('room registered', { isSuccessful: false, error: 'There are too many users in this room!' });
+            if (sockets.length >= 2) {
+                return socket.emit('room joined', { isSuccessful: false, error: 'There are too many users in this room!' });
             }
 
             socket.roomID = roomID;
             socket.join(roomID);
-            socket.emit('room registered', { isSuccessful: true });
 
+            var result = { isSuccessful: true };
+
+            if (sockets.length == 0) {
+                result.message = 'waiting for other player...'
+            }
+
+            socket.emit('room joined', result);
             roomJoined = true;
         });
 
+        /*
         socket.on('chat message', function(message){
             if (!socket.roomID) {
                 return;
@@ -29,6 +36,7 @@ module.exports = function(http) {
 
             io.to(socket.roomID).emit('chat message', message);
         });
+        */
 
         socket.on('disconnect', function(){
             console.log('user disconnected');
