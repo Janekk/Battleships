85210fad44147c0a0466e8203e7fcdc0bf3b198f
Janekk <jkacalak@gmail.com>
2015-01-28 22:56:04 +0100

    chore: delete unused pubsub module (utils/pubsub.js)

--- utils/pubsub.js
+++
@@ -1,9 +0,0 @@
-var pubsub = function(l, u, r, i) {
-  return function(n, f) {
-    r = l[n] = l[n] || [], i = -1;
-    if (f && f.call) r.push(f);
-    else while (r[++i]) r[i].apply(u, arguments);
-  }
-}({});
-
-module.exports = pubsub;
