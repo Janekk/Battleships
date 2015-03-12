2d4676102048373db73e85cc73d84b2deec4dab8
Janekk <jkacalak@gmail.com>
2015-03-12 13:51:41 +0100

    clean up gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -27,25 +27,20 @@ var paths = {
 
 var run = require('gulp-run');
 gulp.task('init', function () {
-
   gulp.src(paths.src.client.fonts)
     .pipe(gulp.dest(paths.dest.client.fonts));
-
-  run('node ./node_modules/jquery-builder/bin/builder.js')
-    .pipe(gulp.dest('public/scripts/jquery.js'));
 });
 
 var argv = require('yargs').argv;
 var uglify = require('gulp-uglify');
 var gulpif = require('gulp-if');
 var buffer = require('vinyl-buffer');
-var production = !!(argv.production); // true if --production flag is used
+var production = !!argv['production']; // true if --production flag is used
 
 gulp.task('browserify', function () {
   browserify({extensions: ['.jsx', '.js']})
     .transform('reactify', {"es6": true}) //don't generate intermediate js files
     .require(paths.src.client.app, { entry: true })
-    .require('react') //chrome tools for React
     .bundle()
     .on('error', function(err){
       console.log(err.message);
@@ -57,7 +52,6 @@ gulp.task('browserify', function () {
 });
 
 var less = require('gulp-less');
-var path = require('path');
 
 gulp.task('less', function () {
   gulp.src('./public/stylesheets/*.less')
@@ -65,7 +59,7 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./public/stylesheets'));
 });
 
-gulp.task('watch-browserify', function () {
+gulp.task('watch-client', function () {
   gulp.watch([paths.src.client.scripts], ['browserify'])
 });
 
